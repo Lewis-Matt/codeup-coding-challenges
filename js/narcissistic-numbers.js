521f5eb41eb1045122f99a7fd265aa61ca7f4ef0
Matthew Lewis <matthew.d.lewis64@gmail.com>
2021-12-28 11:44:05 -0600

    Completed the function, but it does not give accurate return

--- js/narcissistic-numbers.js
+++ js/narcissistic-numbers.js
@@ -12,7 +12,7 @@ Objective: Write a script to generate and output the first 25 narcissistic integ
 
 // Pseudo:
 // Convert the number to a string, then get the length. This is m
-// n is the number itself
+// num is the number itself
 // set up a loop (do - while?) with an if-statement that counts the number of times the condition is passed (25)
 // if (1+5+3)^m == m -->> narcNum ++
 
@@ -20,10 +20,22 @@ function narc(num) {
     if (typeof num !== 'number') {
         return "Enter a number without quotes"
     }
-    let n = num;
+    // Store length of num
     let m = num.toString().length;
-    // Test Log
-    // return m;
+    // Put each digit of the number into an array. The Array.from utility creates a new array from an array-like value or iterable object. It accepts as the second argument a mapping function to call on every element of the array. String(num) is used to convert num to an array-like value (e.g. a string)
+    let numArr = Array.from(String(num), String);
+    let tempArr = [];
+    for (let i = 0; i < numArr.length; i++) {
+        tempArr[i] = parseInt(numArr[i] + m);
+    }
+    console.log(tempArr);
+    let sum = 0;
+    for (let i = 0; i < tempArr.length; i++) {
+        sum += tempArr[i];
+    }
+    console.log(sum);
+    console.log(typeof sum);
+    return sum === num;
 }
 
-console.log(narc(5));
+console.log(narc(153));
