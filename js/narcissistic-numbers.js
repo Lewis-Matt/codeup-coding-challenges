eaaf03cffe63fe0bd39daf4c21df7899bd2a8c6c
Matthew Lewis <matthew.d.lewis64@gmail.com>
2021-12-28 11:53:10 -0600

    Completed funciton. However it takes in a user input, need to add a counter where it increments through each integer, and stops after finding 25 numbers that return true

--- js/narcissistic-numbers.js
+++ js/narcissistic-numbers.js
@@ -6,7 +6,7 @@ A narcissistic number is a non-negative integer (n) with m digits where the sum 
 For example:
 
 If n is 153, then m (the number of digits) is 3 and:
-13 + 53 + 33 = 1 + 125 + 27 = 153
+1^3 + 5^3 + 3^3 = 1 + 125 + 27 = 153
 So, 153 is a narcissistic number.
 Objective: Write a script to generate and output the first 25 narcissistic integers.*/
 
@@ -16,6 +16,7 @@ Objective: Write a script to generate and output the first 25 narcissistic integ
 // set up a loop (do - while?) with an if-statement that counts the number of times the condition is passed (25)
 // if (1+5+3)^m == m -->> narcNum ++
 
+// User inputs a number (num); returns true if it is a narcissistic number
 function narc(num) {
     if (typeof num !== 'number') {
         return "Enter a number without quotes"
@@ -24,17 +25,24 @@ function narc(num) {
     let m = num.toString().length;
     // Put each digit of the number into an array. The Array.from utility creates a new array from an array-like value or iterable object. It accepts as the second argument a mapping function to call on every element of the array. String(num) is used to convert num to an array-like value (e.g. a string)
     let numArr = Array.from(String(num), String);
+    // Create another array to store the results of n^m
     let tempArr = [];
     for (let i = 0; i < numArr.length; i++) {
-        tempArr[i] = parseInt(numArr[i] + m);
+        tempArr[i] = numArr[i] ** m;
     }
-    console.log(tempArr);
+    // Test log
+    // console.log(tempArr);
+
+    // Add the n^m values together
     let sum = 0;
     for (let i = 0; i < tempArr.length; i++) {
         sum += tempArr[i];
     }
-    console.log(sum);
-    console.log(typeof sum);
+    // Test logs
+    // console.log(sum);
+    // console.log(typeof sum);
+
+    // Returns true if the sum equals the original number
     return sum === num;
 }
 
